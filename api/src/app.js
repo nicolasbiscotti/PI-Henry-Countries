ba76b79706caca69a274f5d24eb6e201156bd192
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2021-12-23 12:21:16 -0300

    feat(api/src/routes/index.js): add /countries and /activities endpoints

--- api/src/app.js
+++ api/src/app.js
@@ -33,7 +33,7 @@ server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
-  res.status(status).send(message);
+  res.status(status).json({ from: "server", message });
 });
 
 module.exports = server;
