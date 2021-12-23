c59f95740cc72bb86e18f5dba77a516526d12f76
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2021-12-23 09:46:46 -0300

    feat(models/Country.js): add Country model

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,46 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define("country", {
+    countryId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    flagURI: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.REAL,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+    },
+    areaKm2: {
+      type: DataTypes.VIRTUAL,
+      get() {
+        return `${this.area} km2`;
+      },
+      set(value) {
+        throw new Error("Do not try to set the `areaKm2` value!");
+      },
     },
   });
 };
