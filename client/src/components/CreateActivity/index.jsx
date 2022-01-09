f3e25a90cc488c5b0becaebc8bb51935b9b721b2
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-09 11:25:18 -0300

    feat: activity form, you can create new activities

--- client/src/components/CreateActivity/index.jsx
+++ client/src/components/CreateActivity/index.jsx
@@ -1,9 +1,12 @@
 import Topbar from "../Topbar";
+import ActivityForm from "../ActivityForm";
 import { StyledCreateActivity } from "./styled";
 
 export default function CreateActivity() {
-  return <StyledCreateActivity>
-    <Topbar />
-    
-  </StyledCreateActivity>;
+  return (
+    <StyledCreateActivity>
+      <Topbar />
+      <ActivityForm />
+    </StyledCreateActivity>
+  );
 }
