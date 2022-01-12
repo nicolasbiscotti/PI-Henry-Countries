c4679cac4da7e7c6b2c8b25ef79b366cc996e4b4
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-12 07:26:13 -0300

    refactor(api/src/utils/index.js): load countries and activities only if necessary

--- api/index.js
+++ api/index.js
@@ -20,12 +20,12 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { PORT } = process.env;
-const { fetchCountries } = require("./src/utils");
+const { loadCountries } = require("./src/utils");
 
 // Syncing all the models at once.
 conn
-  .sync({ force: true })
-  .then(() => fetchCountries())
+  .sync({ force: false })
+  .then(() => loadCountries())
   .then(() => {
     server.listen(PORT, () => {
       console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
