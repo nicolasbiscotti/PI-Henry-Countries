5e495d3a30949b92e0e01d009d017994141cac84
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-01-12 09:35:17 -0300

    fix(ActivityForm.js): base url

--- client/src/components/CreateActivity/styled.js
+++ client/src/components/CreateActivity/styled.js
@@ -2,9 +2,6 @@ import styled from "styled-components";
 
 export const StyledCreateActivity = styled.div`
   min-height: 100vh;
-  background-color: #ffd200;
   background-image: url("https://images.pexels.com/photos/1464143/pexels-photo-1464143.jpeg?auto=compress&cs=originalsrgb&dpr=3&h=5211&w=7813");
   background-size: cover;
-  background-repeat: no-repeat;
-  background-position-x: right;
 `;
